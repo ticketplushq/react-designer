568bead6a6213d3f5bb0c69600ff0b9d3e754320
andresdev <andresmestra11a@gmail.com>
2022-02-07 00:30:54 -0500

    build: implement css-modules

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === 'development'
+
+  return {
+    plugins: [react()],
+    css: {
+      modules: {
+        generateScopedName: isDevelopment
+          ? '[name]__[local]__[hash:base64:5]'
+          : '[hash:base64:5]',
+      },
+    },
+  }
 })
