20f22204abc659f1774818e918777c396c2b1331
andresdev <andresmestra11a@gmail.com>
2022-03-25 16:06:56 -0500

    build: lib package with webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/designer/index.js',
+  externals: [nodeExternals()],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+    libraryTarget: 'commonjs',
+  },
+  plugins: [new CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            '@babel/preset-env',
+            ['@babel/preset-react', { runtime: 'automatic' }],
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+}
