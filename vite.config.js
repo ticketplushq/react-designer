ea91a473f274d1f682656601fd3bd7822615fd6b
andresmestradev <andresmestra11a@gmail.com>
2023-07-09 22:04:26 -0500

    feat: set base url and title for page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    base: isDevelopment ? '/' : '/react-designer',
     css: {
       modules: {
         generateScopedName: isDevelopment
